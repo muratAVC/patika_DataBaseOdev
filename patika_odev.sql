897d7c08f0500a574ab43f71e9867de1dbe33ca3
muratAVC <110787002+muratAVC@users.noreply.github.com>
2023-06-17 18:25:48 +0300

    Update patika_odev.sql

--- patika_odev.sql
+++ patika_odev.sql
@@ -1,3 +1,4 @@
+--ödev 4
 --film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
 SELECT distinct replacement_cost from film;
 --film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
@@ -12,3 +13,13 @@ SELECT * from city;
 SELECT count(city) from city where city.city like '%r' or city like 'R';
 --veya kısa yöntemi
 SELECT count(city) from city WHERE city.city ilike '%r';
+
+--Ödev 5 
+
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+SELECT * from film where title like '%n' order by length DESC limit 5;
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+SELECT * from film where title like '%n' order by length ASC offset 5 limit 5;
+--customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada
+-- store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+SELECT * from customer where store_id=1 order by last_name DESC limit 4;
