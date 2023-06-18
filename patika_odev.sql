3f1fc145f03f127ddc31682f47cf4a710db4ee18
muratAVC <110787002+muratAVC@users.noreply.github.com>
2023-06-18 09:51:38 +0300

    Update patika_odev.sql ödev 7

--- patika_odev.sql
+++ patika_odev.sql
@@ -25,6 +25,7 @@ SELECT * from film where title like '%n' order by length ASC offset 5 limit 5;
 SELECT * from customer where store_id=1 order by last_name DESC limit 4;
 
 
+
 --ödev6
 --film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
 SELECT avg(rental_rate) from film;
@@ -35,3 +36,16 @@ SELECT max(length) from film where rental_rate=0.99;
 --film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır
 SELECT count(distinct replacement_cost) from film where length>150;
 
+
+
+--ödev7 
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating from film group by rating;
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda
+    -- film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, count(replacement_cost) from film group by replacement_cost having count(replacement_cost)>50;
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+SELECT store_id,count(store_id) from customer group by store_id;
+-- city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra
+    -- en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+select country_id, count(city) from city group by country_id order by count(country_id) DESC limit 1;
